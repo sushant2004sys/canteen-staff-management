60a5309086a241c5e7b4bfb8b34089b26cb634e1
Sushant Kulkarni <sushantkulkarni728@gmail.com>
2026-07-03 12:03:21 +0530

    Fixed static settings

--- canteen/settings.py
+++ canteen/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
