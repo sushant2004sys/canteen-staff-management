caa583532f6236dad184cf9a79b1e88d9446a046
Sushant Kulkarni <sushantkulkarni728@gmail.com>
2026-07-03 11:51:49 +0530

    Changed database to SQLite

--- canteen/settings.py
+++ canteen/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'canteen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'canteen_staff',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
